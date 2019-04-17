62cde55ecdf38d8f791a87147bb225dfd87a7bce
dwillington <dwillington@yahoo.com>
2019-04-16 22:09:04 -0400

    commit

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,10 +1,10 @@
 application_name=hello-world-war
 
 if [[ $(docker ps -a | grep $application_name | awk '{ print $1 }') ]]; then
-    echo "$application_name container already exists, update application"
+    docker stop $(docker ps -a | grep $application_name | awk '{ print $1 }')
+	docker rm $(docker ps -a | grep $application_name | awk '{ print $1 }')
 else
-    echo "$application_name container does not exist, create container, add application"
-	docker run -dit --rm -p 80:8080 --name $application_name tomcat
 fi
+docker run -dit --rm -p 80:8080 --name $application_name tomcat
 docker cp hello-world-war-*.war $(docker ps -a | grep $application_name | awk '{ print $1 }'):/usr/local/tomcat/webapps/.
 docker logs --tail=100 $(docker ps -a | grep $application_name | awk '{ print $1 }')
